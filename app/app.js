91fdc2229ca6cc5db9b746f5124cc446508c1a8d
Jasmine Quach <xjasminequach@gmail.com>
2017-09-06 21:41:23 -0500

    WIP modal

--- app/app.js
+++ app/app.js
@@ -50,3 +50,12 @@ app.run(($location, FBCreds) => {
     };
     firebase.initializeApp(authConfig);
 });
+
+//modal
+
+$(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+      $('.modal').modal();
+    });
+      $('#modal1').modal('open');
+      $('#modal1').modal('close');
