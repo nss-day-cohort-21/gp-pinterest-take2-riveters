ee5d29ac354500638713f8b482441eca1f5e040b
Brooke Wittenberg <brooke.wittenberg@gmail.com>
2017-09-07 23:48:32 -0500

    fixed the button in add rivet form, fixed formtitle in add pin controller

--- app/controllers/addpins.controller.js
+++ app/controllers/addpins.controller.js
@@ -2,31 +2,29 @@
 
 // console.log('Add Pins.controller');
 
-app.controller("addPin", function ($scope, $location, $routeParams, UserBoards, authFactory, $route) {
-    
-        $scope.title = "New Pin";
-        $scope.submitButtonText = "Add New Pin";
+app.controller("addPin", function($scope, $location, $routeParams, UserBoards, authFactory, $route) {
 
-        let user = authFactory.getCurrentUser();
-    
-        $scope.pin = {
-            title: "",
-            uid: user,
-            img: "",
-            description: "",
-            tags: "",
-            url: "",
-            boardid: $routeParams.itemId
-        };
-    
-        $scope.submitPin = function () {
-            console.log("WHYYY");
-            UserBoards.addNewPin($scope.pin)
-                .then((data) => {
-                    $location.url("#!/board/{{item.id}}");
-                    $route.reload();
-                });
-        };
-    });
+    $scope.title = "New Rivet";
+    $scope.submitButtonText = "Add";
 
+    let user = authFactory.getCurrentUser();
 
+    $scope.pin = {
+        title: "",
+        uid: user,
+        img: "",
+        description: "",
+        tags: "",
+        url: "",
+        boardid: $routeParams.itemId
+    };
+
+    $scope.submitPin = function() {
+        console.log("WHYYY");
+        UserBoards.addNewPin($scope.pin)
+            .then((data) => {
+                $location.url("#!/board/{{item.id}}");
+                $route.reload();
+            });
+    };
+});
